90dd2fd361eef912f8ad2c0bfc79ebe95566185e
silentesc <scope-frisk-copy@duck.com>
2024-07-10 21:11:10 +0200

    Refactoring and some improvements

---
+++ src/anti_afk.rs
@@ -0,0 +1,53 @@
+use std::{io::Write, time::Duration};
+
+use enigo::{Enigo, Keyboard, Settings};
+
+pub fn start_anti_afk() {
+    // Print a message to the console
+    println!(
+        "
+.·:'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''':·.
+: :     _          _   _    _    _____ _  __         ___   _   ___   : :
+: :    / \\   _ __ | |_(_)  / \\  |  ___| |/ / __   __/ _ \\ / | / _ \\  : :
+: :   / _ \\ | '_ \\| __| | / _ \\ | |_  | ' /  \\ \\ / / | | || || | | | : :
+: :  / ___ \\| | | | |_| |/ ___ \\|  _| | . \\   \\ V /| |_| || || |_| | : :
+: : /_/   \\_\\_| |_|\\__|_/_/   \\_\\_|   |_|\\_\\   \\_/  \\___(_)_(_)___/  : :
+'·:..................................................................:·'
+    "
+    );
+
+    const SLEEP_SECONDS: u64 = 1;
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    let mut counter = 1;
+
+    println!(
+        "This program will simulate a key press every {} minutes to prevent AFK.",
+        SLEEP_SECONDS / 60
+    );
+    println!("");
+
+    loop {
+        let current_time = chrono::Local::now().time().format("%H:%M:%S").to_string();
+
+        // Simulate a key release
+        match enigo.key(enigo::Key::LWin, enigo::Direction::Release) {
+            Ok(_) => (),
+            Err(e) => eprintln!("\r[{}] Error releasing key: {}\x1B[K", current_time, e),
+        }
+
+        // Print info
+        print!(
+            "\rTriggered anti afk {} times (Last trigger: {})\x1B[K",
+            counter, current_time
+        );
+        match std::io::stdout().flush() {
+            Ok(_) => (),
+            Err(e) => eprintln!("[{}] Error flushing stdout: {}", current_time, e),
+        }
+
+        counter += 1;
+
+        // Sleep before next key press
+        std::thread::sleep(Duration::from_secs(SLEEP_SECONDS));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,5 @@
-use std::{io::Write, time::Duration};
-
-use enigo::{Enigo, Keyboard, Settings};
+pub mod anti_afk;
 
 fn main() {
-    // Print a message to the console
-    println!("");
-    println!("    _          _   _      _    _____ _  __         ___   _   ___  ");
-    println!("   / \\   _ __ | |_(_)    / \\  |  ___| |/ / __   __/ _ \\ / | / _ \\ ");
-    println!("  / _ \\ | '_ \\| __| |   / _ \\ | |_  | ' /  \\ \\ / / | | || || | | |");
-    println!(" / ___ \\| | | | |_| |  / ___ \\|  _| | . \\   \\ V /| |_| || || |_| |");
-    println!("/_/   \\_\\_| |_|\\__|_| /_/   \\_\\_|   |_|\\_\\   \\_/  \\___(_)_(_)___/ ");
-    println!("");
-
-    const SLEEP_SECONDS: u64 = 1;
-    let mut enigo = Enigo::new(&Settings::default()).unwrap();
-    let mut counter = 0;
-
-    println!(
-        "This program will simulate a key press every {} minutes to prevent AFK.",
-        SLEEP_SECONDS / 60
-    );
-    println!("");
-
-    loop {
-        let current_time = chrono::Local::now().time().format("%H:%M:%S").to_string();
-
-        // Simulate a key release
-        match enigo.key(enigo::Key::LWin, enigo::Direction::Release) {
-            Ok(_) => (),
-            Err(e) => eprintln!("\r[{}] Error releasing key: {}\x1B[K", current_time, e),
-        }
-
-        // Print info
-        print!(
-            "\rTriggered anti afk {} times (Last trigger: {})\x1B[K",
-            counter, current_time
-        );
-        match std::io::stdout().flush() {
-            Ok(_) => (),
-            Err(e) => eprintln!("[{}] Error flushing stdout: {}", current_time, e),
-        }
-
-        counter += 1;
-
-        // Sleep before next key press
-        std::thread::sleep(Duration::from_secs(SLEEP_SECONDS));
-    }
+    anti_afk::start_anti_afk();
 }
