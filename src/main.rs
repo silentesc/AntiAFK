b135e47ec5753c5cdb209562544c34918177a506
silentesc <scope-frisk-copy@duck.com>
2026-01-08 20:23:30 +0100

    replace key press logic with keepawake create which basically does everything for you

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-pub mod anti_afk;
+use std::{thread, time::Duration};
+
+use keepawake::Builder;
 
 fn main() {
-    anti_afk::start_anti_afk();
+    let _awake = Builder::default()
+        .display(true)
+        .idle(true)
+        .sleep(true)
+        .create()
+        .expect("Failed to prevent sleep");
+
+    println!("PC will stay awake until this program is closed.");
+
+    loop {
+        thread::sleep(Duration::from_secs(60));
+    }
 }
